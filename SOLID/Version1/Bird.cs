87126f49f562a8f29bd08a2a439c870908fa7964
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-25 07:53:54 +0530

    completed version 1

--- SOLID/Version1/Bird.cs
+++ SOLID/Version1/Bird.cs
@@ -8,5 +8,43 @@ namespace SOLID.Version1
 {
     internal class Bird
     {
+        public string Name { get; set; }
+        public BirdType birdType { get; set; }
+        public int weight { get; set; }
+        public BirdSize size { get; set; }
+
+        public void fly()
+        {
+            Console.WriteLine("flap !!!");
+        }
+
+        public void eat()
+        {
+            Console.WriteLine("Yummy !!!");
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine("Zzzzzz !!!");
+        }
+
+        public void fight()
+        {
+            Console.WriteLine("WTF !!!");
+        }
+    }
+
+    internal enum BirdSize
+    {
+        small,
+        medium,
+        large
+    }
+
+    internal enum BirdType
+    {
+        Egle,
+        sparrow,
+        parrot
     }
 }
