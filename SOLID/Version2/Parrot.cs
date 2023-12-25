d0ebd32e9100fb5d9ad92388316afbf0b4d2301f
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-25 08:11:09 +0530

    Fixed bad code by creating separate class of types and inherite from base abstract bird class

---
+++ SOLID/Version2/Parrot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Version2
+{
+    internal class Parrot : Bird
+    {
+        public override void fly()
+        {
+            Console.WriteLine("flap !!!");
+        }
+    }
+}
