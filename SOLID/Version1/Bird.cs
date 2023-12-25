334953bf9ad9294d429597d61a18fd7e2b72949a
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-25 07:36:23 +0530

    Created new folder for version 1

---
+++ SOLID/Version1/Bird.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Version1
+{
+    internal class Bird
+    {
+    }
+}
