bc304207a1052a55228c2d80aa6cf56eaaaa88a3
sibisaravanan94_69ee6ac0e1c0-org <sibisaravanan94@gmail.com>
2023-12-25 09:12:07 +0530

    Found bad code, fat interface, Interface should have high cohesion and low coupling

---
+++ SOLID/Version5/Bird.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Version5
+{
+    internal abstract class Bird
+    {
+        public string Name { get; set; }
+        public BirdType birdType { get; set; }
+        public int weight { get; set; }
+        public BirdSize size { get; set; }
+
+        public void eat()
+        {
+            Console.WriteLine("Yummy !!!");
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine("Zzzzzz !!!");
+        }
+
+        public void fight()
+        {
+            Console.WriteLine("WTF !!!");
+        }
+    }
+
+    internal enum BirdSize
+    {
+        small,
+        medium,
+        large
+    }
+
+    internal enum BirdType
+    {
+        Eagle,
+        sparrow,
+        parrot,
+        penguin
+    }
+}
---
+++ SOLID/Version5/Flyable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Version5
+{
+    // Breaking single responsibility, Liskov substitution and interface segrigation
+    // fat/ bulk interface
+    // it doesn't say SAM
+    // high cohesion and low coupling
+    internal interface Flyable
+    {
+        void fly();
+        void sing();
+        void swim();
+    }
+}
